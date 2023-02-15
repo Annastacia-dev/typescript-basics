30bdc9f90278b625a785b130722a46948dae520e
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-02-15 09:04:10 +0300

    tuples

---
+++ tuples.ts
@@ -0,0 +1,20 @@
+// Tuples - Array-like structure.
+
+const drink: (number | string )[] = [21, 'brown', 'sweet'] 
+
+// If you want to have the typrs in a specific order, use a tuple
+
+const newDrink: [number, string, boolean] = [21, 'brown', true] // This is a tuple, you can't reverse the order - useful for API calls
+
+let rgb: [number, number, number] = [255, 255, 255]
+
+type Student = [number, string]
+
+//const student: Student = ['John', 21] // This is wrong, the order is wrong, error
+const student2: Student = [21, 'John'] // This is correct
+
+//student2[0] = '22' // errror - type 'string' is not assignable to type 'number'
+
+//student2.push(true) //error -  argument type 'true' is not assignable to parameter type 'string | number'
+
+console.log(student2)
